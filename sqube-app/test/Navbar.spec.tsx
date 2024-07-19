e3cbfd9e470f090887751ba53d4a4179ad5f99b0
Jan Luca Sterta <janluca.sterta@dxc.com>
2024-07-19 16:06:22 +0200

    Change lcov.info path depending on given working directory path

--- sqube-app/test/Navbar.spec.tsx
+++ sqube-app/test/Navbar.spec.tsx
@@ -16,4 +16,10 @@ test("loads and displays Navbar component", async () => {
   expect(screen.getByText("BDD")).toBeInTheDocument();
   expect(screen.getByText("NNA")).toBeInTheDocument();
   expect(screen.getByText("CM")).toBeInTheDocument();
+
+  //Assert that when linkas are clicked, the correct page is displayed
+  expect(screen.getByText("Home")).toHaveAttribute("href", "/");
+  expect(screen.getByText("Docs")).toHaveAttribute("href", "/docs");
+  expect(screen.getByText("BDD")).toHaveAttribute("href", "/b");
+  expect(screen.getByText("NNA")).toHaveAttribute("href", "/nna");
 });
