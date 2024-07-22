fd43831a1adbceae1fed8b3b2d461a106bfac193
Jan Luca Sterta <janluca.sterta@dxc.com>
2024-07-22 17:51:34 +0200

    Update vite.config.ts for code coverage improvements

--- sqube-app/vite.config.ts
+++ sqube-app/vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
+/// <reference types="vitest/globals" />
 
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from "@vitejs/plugin-react";
+import '@testing-library/jest-dom/vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,8 +14,9 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     coverage: {
-      reporter: 'lcovonly',
-    }
+      reporter: ["text", "lcov"],
+      exclude: ["**/.eslintrc.cjs"],
+    },
   },
   resolve: {
     alias: {
