5abda2df3b6951af529493ea30b7de7822eee029
Jan Luca Sterta <janluca.sterta@dxc.com>
2024-07-24 09:51:10 +0200

    Add coverage configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import react from "@vitejs/plugin-react";
+import { defineConfig, mergeConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig as defineTestConfig } from "vitest/config";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  cacheDir: ".vite",
+const baseConfig = defineConfig({
+  plugins: [react(), tsconfigPaths()]
 });
 
+const testConfig = defineTestConfig({
+  test: {
+    coverage: {
+      reportsDirectory: "coverage",
+      reporter: ["lcov", "html", "text"],
+      all: true,
+      include: ["src"],
+      exclude: ["**/*.{test,spec}.?(c|m)[jt]s?(x)", "**/*.d.ts", "src/main.ts?(x)"],
+    },
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+  },
+});
+
+export default mergeConfig(baseConfig, testConfig);
