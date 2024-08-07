c38c465f0e396b26c64107634a9527ac7e852924
Jan Luca Sterta <janluca.sterta@dxc.com>
2024-08-07 10:36:15 +0200

    Add standard binary diagram look

--- src/components/modules/binarydiagram/BinaryDiagram.tsx
+++ src/components/modules/binarydiagram/BinaryDiagram.tsx
@@ -6,7 +6,7 @@ import LOGGER from "@utils/Logger";
 
 /** Function to create a new node */
 const createNode = (row: CSVRowData, level: number): TreeNode => ({
-  name: row[`Part Level ${String(level)}` as keyof CSVRowData],
+  name: row[`Part Level ${String(level)}` as keyof CSVRowData].trim(),
   attributes: {
     picture: row.Picture,
     materialType: row["Material Type(s)"],
@@ -17,7 +17,7 @@ const createNode = (row: CSVRowData, level: number): TreeNode => ({
   children: [],
 });
 
-/** Function to process rows and build the tree */
+/** Function to process rows and build the tree, creates a new Tree object */
 const processRows = (rows: CSVRowData[], fileName: string): TreeNode => {
   const tree: TreeNode = {
     name: fileName,
@@ -33,27 +33,27 @@ const processRows = (rows: CSVRowData[], fileName: string): TreeNode => {
 
   const addRowToTree = (tree: TreeNode, row: CSVRowData) => {
     let currentNode = tree;
+    // Number of root levels / part levels in excel sheets, including root level zero
+    const rootLevels = 4;
 
-    for (let level = 0; level <= 4; level++) {
-      const partLevel = row[`Part Level ${String(level)}` as keyof CSVRowData];
-      LOGGER.info(`Processing level ${String(level)}: ${partLevel}`);
+    for (let level = 0; level <= rootLevels; level++) {
+      const part =
+        row[`Part Level ${String(level)}` as keyof CSVRowData].trim();
 
-      if (!partLevel) break; // If the part level is empty, that's the correct level to stop
+      if (!part) break; // If the part level is empty, that's the correct level to stop
 
-      let childNode = currentNode.children.find(
-        (child) => child.name === partLevel
-      );
-
-      LOGGER.info(partLevel);
+      // Saves the predicate value if found and undefined otherwise
+      let childNode = currentNode.children.find((child) => child.name === part);
 
       if (childNode) {
         LOGGER.info(`Found existing node: ${childNode.name}`);
       } else {
-        LOGGER.info(`Creating new node for: ${partLevel}`);
+        // Create and push new node if no matching childnode was found
         childNode = createNode(row, level);
         currentNode.children.push(childNode);
       }
 
+      // Move to the next level
       currentNode = childNode;
     }
   };
@@ -70,6 +70,7 @@ const BinaryDiagram: React.FC = () => {
 
   const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files === null) return;
+
     const file = event.target.files[0];
     Papa.parse(file, {
       header: true,
@@ -80,17 +81,30 @@ const BinaryDiagram: React.FC = () => {
         setTreeData(tree);
       },
       error: function (error) {
-        LOGGER.info("Error parsing CSV:", error); //use a custom LOGGER instead
+        LOGGER.info("Error occured while trying to parse CSV:", error); //use a custom LOGGER instead
       },
     });
   };
+
+  const nodeSize = { x: 200, y: 200 };
+  const seperation = { siblings: 2, nonSiblings: 1.5 };
 
   return (
     <div className="w-full h-full">
       <input type="file" name="file" accept=".csv" onChange={handleFileInput} />
       {treeData ? (
         <div id="treeWrapper" style={{ width: "100vw", height: "100vh" }}>
-          <Tree data={treeData} />
+          <Tree
+            data={treeData}
+            rootNodeClassName="node__root"
+            branchNodeClassName="node__branch"
+            leafNodeClassName="node__leaf"
+            orientation="vertical"
+            nodeSize={nodeSize}
+            separation={seperation}
+            translate={{ x: 100, y: 100 }}
+            pathFunc="step"
+          />
         </div>
       ) : (
         <span>Only CSV files are supported</span>
